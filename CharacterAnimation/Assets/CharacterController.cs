23e545fe219d9af04e3971662be65f8c09a6dac8
Moon Sungphill <sp9103@naver.com>
2016-07-07 16:15:05 +0900

    *Initial project commit

---
+++ CharacterAnimation/Assets/CharacterController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+    public GameObject character;
+    public bool ReadFromFile;
+    public int FrameRate = 30;
+    public string FilePath;
+
+    private float waitInterval;
+
+	// Use this for initialization
+	void Start () {
+        if (FrameRate <= 0) FrameRate = 30;
+
+        waitInterval = 1000.0f / (float)FrameRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //파일에서부터 읽어들일때
+        if (ReadFromFile)
+        {
+
+        }
+        else        //통신으로부터 직접받아올
+        {
+            //TO-DO
+        }
+	}
+}
